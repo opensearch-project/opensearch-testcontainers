4206d81e70bb47b78f868cc31078e0ed07c1ab8a
Andriy Redko <andriy.redko@aiven.io>
2022-10-27 22:55:52 -0400

    Add version.properties and refactor version management at build time (#36)
    * Add version.properties and refactor version management at build time
    
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>
    
    * Addressing code review comments
    
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>
    
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,9 @@
  */
 
 import net.researchgate.release.ReleaseExtension
+import java.io.File
+import java.io.FileInputStream
+import java.util.Properties
 
 plugins {
   java
@@ -47,12 +50,26 @@ dependencies {
 }
 
 group = "org.opensearch"
-var isSnapshot = "true" == System.getProperty("build.snapshot", "true")
-var version_temp = "2.0.1"
-if (isSnapshot) {
-    version_temp += "-SNAPSHOT"
+val build = Properties().apply {
+  load(FileInputStream(File(rootProject.rootDir, "version.properties")))
 }
-version = version_temp
+
+// Detect version from version.properties and align it with the build settings
+var isSnapshot = "true" == System.getProperty("build.snapshot", "true")
+var buildVersion = build.getProperty("version")
+if (isSnapshot && !buildVersion.endsWith("SNAPSHOT")) {
+  buildVersion = buildVersion + "-SNAPSHOT"
+} else if (!isSnapshot && buildVersion.endsWith("SNAPSHOT")) {
+  throw GradleException("Expecting release (non-SNAPSHOT) build but version is not set accordingly: " + buildVersion)
+}
+  
+// Check if tag release version (if provided) matches the version from build settings 
+val tagVersion = System.getProperty("build.version", buildVersion)
+if (!buildVersion.equals(tagVersion)) {
+  throw GradleException("The tagged version " + tagVersion + " does not match the build version " + buildVersion)
+}
+
+version = buildVersion 
 description = "Testcontainers for Opensearch"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
